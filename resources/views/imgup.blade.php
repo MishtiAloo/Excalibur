4abbbd7d5b01d00734eb62500c0fa2bd344e5e7c
f.tahmid123@gmail.com <f.tahmid123@gmail.com>
2025-10-22 19:02:10 +0600

    Cleaned some unnecessary things

--- resources/views/imgup.blade.php
+++
@@ -1,8 +0,0 @@
-<!DOCTYPE html>
-<html>
-  <body>
-    <x-cloudinary::widget>Upload Files</x-cloudinary::widget>
-
-    <x-cloudinary::image public-id="WhatsApp_Image_2025-06-19_at_22.19.35_08b55951_p9ea5x" />    
-  </body>
-</html>
